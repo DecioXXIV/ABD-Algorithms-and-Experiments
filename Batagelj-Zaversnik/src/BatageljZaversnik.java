30a3ab21c275a2c35fc4b4ba25a9f19300cf02c4
Riccardo De Cesaris <ric.dc@hotmail.it>
2023-11-27 13:23:59 +0100

    Commit "Batagelj-Zaversnik Algorithm"

--- Batagelj-Zaversnik/src/BatageljZaversnik.java
+++ Batagelj-Zaversnik/src/BatageljZaversnik.java
@@ -1,15 +1,141 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class BatageljZaversnik {
 
 	public Map<Integer, List<Integer>> graph;
+	public int nodes;
+	public Integer[] degree;
+	public Integer[] bin;
+	public Integer[] sort;
+	public Integer[] pos;
 	
 	public BatageljZaversnik(Graph graph) {
 		this.graph = graph.getGraph();
+		this.nodes = this.graph.keySet().size();
+		
+		this.degree = new Integer[this.nodes];
+		this.sort = new Integer[this.nodes];
+		this.pos = new Integer[this.nodes];
+		this.bin = null;
 	}
 	
-	public void BTAlgorithm() {
-		// TODO
+	public void BZAlgorithm() {
+		
+		// Step 1: inizializzazione
+		computeDegree();
+		buildBin();
+		buildSortAndPos();
+		resetBin();
+		
+		// Step 2: esecuzione dell'Algoritmo
+		for (int i=0; i < this.sort.length; i++) {
+			int node = this.sort[i];
+			int nodeDegree = this.degree[node];
+						
+			for (int neighbor: this.graph.get(node)) {
+				int neighborDegree = this.degree[neighbor];
+				int neighborPos = this.pos[neighbor];
+				if (neighborDegree > nodeDegree) {
+					
+					int firstNodeInTheBucket = this.sort[this.bin[neighborDegree]];
+					int firstNodeInTheBucketPos = this.pos[firstNodeInTheBucket];
+					
+					// Scambio in 'sort'
+					int temp = this.sort[neighborPos];
+					this.sort[neighborPos] = this.sort[firstNodeInTheBucketPos];
+					this.sort[firstNodeInTheBucketPos] = temp;
+					
+					// Scambio in 'pos'
+					temp = this.pos[neighbor];
+					this.pos[neighbor] = this.pos[firstNodeInTheBucket];
+					this.pos[firstNodeInTheBucket] = temp;
+					
+					// Aggiornamento di 'bin'
+					this.bin[neighborDegree] = this.bin[neighborDegree] + 1;
+					
+					// Aggiornamento di 'degree'
+					this.degree[neighbor] = neighborDegree - 1;
+				}
+			}
+		}
+		
+		// Step 3: ritornare i risultati
+		System.out.println("*** FINE DELLA RICERCA ***");
+		
+		Set<Integer> core = new HashSet<>(this.graph.keySet());
+		int coreness = 0;
+		while (core.size() > 0) {
+			for (int node = 0; node < this.degree.length; node++) {
+				if (this.degree[node] < coreness)
+					core.remove(node);
+			}
+			System.out.println("Nodi che fanno parte del " + coreness + "-core: " + core);
+			coreness++;
+		}
+		System.out.println("\n");
+	}
+	
+	private void computeDegree() {
+
+		for (int i=0; i < this.degree.length; i++) {
+			Integer neighbors = this.graph.get(i).size();
+			this.degree[i] = neighbors;
+		}
+	}
+	
+	private void buildBin() {
+		int maxDegree = Integer.MIN_VALUE;
+		
+		for (int i=0; i < this.degree.length; i++) {
+			if (this.degree[i] > maxDegree)
+				maxDegree = this.degree[i];
+		}
+		
+		this.bin = new Integer[maxDegree+1];
+		
+		for (int i=0; i < this.bin.length; i++) {
+			int numNodes = 0;
+			for (int j=0; j < this.degree.length; j++) {
+				if (this.degree[j] == i)
+					numNodes++;
+			}
+			this.bin[i] = numNodes;
+		}
+		
+		int start = 0;
+		int num;
+		for (int d=0; d <= maxDegree; d++) {
+			num = this.bin[d];
+			this.bin[d] = start;
+			start = start + num;
+		}
+	}
+	
+	private void buildSortAndPos() {
+		for (int i=0; i < this.degree.length; i++) {
+			int degree = this.degree[i];
+			int position = this.bin[degree];
+			
+			this.sort[position] = i;
+			this.pos[i] = position;
+			
+			this.bin[degree] = this.bin[degree] + 1;
+		}
+	}
+	
+	private void resetBin() {
+		int maxDegree = Integer.MIN_VALUE;
+		for (int i=0; i < this.degree.length; i++) {
+			if (this.degree[i] > maxDegree)
+				maxDegree = this.degree[i];
+		}
+		
+		for (int d = maxDegree; d > 0; d--) {
+			this.bin[d] = this.bin[d-1];
+		}
+		this.bin[0] = 0;
 	}
 }
--- Batagelj-Zaversnik/src/Main.java
+++ Batagelj-Zaversnik/src/Main.java
@@ -2,42 +2,121 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Graph graph = new Graph();
 		
-		graph.addEdge(1, 2);
-		graph.addEdge(1, 3);
-
-		graph.addEdge(2, 3);
-		graph.addEdge(2, 4);
-		graph.addEdge(2, 10);
-		graph.addEdge(2, 11);
-		graph.addEdge(2, 14);
-
-		graph.addEdge(3, 4);
-		graph.addEdge(3, 5);
-		graph.addEdge(3, 6);
-		graph.addEdge(3, 7);
-
-		graph.addEdge(5, 6);
-		graph.addEdge(5, 7);
-		graph.addEdge(5, 9);
-
-		graph.addEdge(6, 7);
-		graph.addEdge(6, 8);
-		graph.addEdge(6, 9);
-
-		graph.addEdge(10, 11);
-		graph.addEdge(10, 12);
-		graph.addEdge(10, 13);
-		graph.addEdge(10, 14);
-
-		graph.addEdge(11, 12);
-
-		graph.addEdge(11, 13);
-
-		System.out.println(graph.toString());
+		/* First Graph */
+		Graph graph1 = new Graph();
 		
-		BatageljZaversnik bz = new BatageljZaversnik(graph);
-		bz.BTAlgorithm();	//TODO
+		graph1.addEdge(0, 1);
+		graph1.addEdge(0, 2);
+
+		graph1.addEdge(1, 2);
+		graph1.addEdge(1, 3);
+		graph1.addEdge(1, 9);
+		graph1.addEdge(1, 10);
+		graph1.addEdge(1, 13);
+
+		graph1.addEdge(2, 3);
+		graph1.addEdge(2, 4);
+		graph1.addEdge(2, 5);
+		graph1.addEdge(2, 6);
+
+		graph1.addEdge(4, 5);
+		graph1.addEdge(4, 6);
+		graph1.addEdge(4, 8);
+
+		graph1.addEdge(5, 6);
+		graph1.addEdge(5, 7);
+		graph1.addEdge(5, 8);
+
+		graph1.addEdge(9, 10);
+		graph1.addEdge(9, 11);
+		graph1.addEdge(9, 12);
+		graph1.addEdge(9, 13);
+
+		graph1.addEdge(10, 11);
+		graph1.addEdge(10, 12);
+		
+		System.out.println(graph1.toString());
+		
+		BatageljZaversnik bz = new BatageljZaversnik(graph1);
+		bz.BZAlgorithm();
+		
+		/* Second Graph */
+		
+		Graph graph2 = new Graph();
+		
+		graph2.addEdge(0, 4);
+		
+		graph2.addEdge(1, 2);
+		
+		graph2.addEdge(2, 3);
+		graph2.addEdge(2, 4);
+		
+		graph2.addEdge(4, 5);
+		graph2.addEdge(4, 6);
+		graph2.addEdge(4, 7);
+		graph2.addEdge(4, 8);
+		
+		graph2.addEdge(5, 6);
+		graph2.addEdge(5, 7);
+		graph2.addEdge(5, 8);
+		
+		graph2.addEdge(6, 7);
+		graph2.addEdge(6, 8);
+		
+		graph2.addEdge(7, 8);
+		
+		graph2.addEdge(8, 9);
+		
+		graph2.addEdge(9, 10);
+		graph2.addEdge(9, 11);
+		
+		graph2.addEdge(10, 11);
+		graph2.addEdge(10, 12);
+		graph2.addEdge(10, 13);
+		graph2.addEdge(10, 14);
+
+		graph2.addEdge(11, 12);
+		graph2.addEdge(11, 13);
+		graph2.addEdge(11, 14);
+		
+		graph2.addEdge(12, 13);
+		graph2.addEdge(12, 14);
+		graph2.addEdge(12, 15);
+		
+		graph2.addEdge(13, 14);
+		graph2.addEdge(13, 16);
+		
+		graph2.addEdge(15, 16);
+		
+		graph2.addEdge(16, 17);
+		graph2.addEdge(16, 18);
+		
+		graph2.addEdge(17, 19);
+		
+		graph2.addEdge(18, 19);
+		graph2.addEdge(18, 23);
+		graph2.addEdge(18, 24);
+		
+		graph2.addEdge(19, 20);
+		graph2.addEdge(19, 21);
+		graph2.addEdge(19, 22);
+		graph2.addEdge(19, 24);
+		
+		graph2.addEdge(22, 24);
+		graph2.addEdge(22, 25);
+		
+		graph2.addEdge(23, 26);
+		graph2.addEdge(23, 24);
+		
+		graph2.addEdge(24, 25);
+		graph2.addEdge(24, 26);
+		
+		graph2.addEdge(25, 26);
+
+		System.out.println(graph2.toString());
+		
+		bz = new BatageljZaversnik(graph2);
+		bz.BZAlgorithm();
 	}
 }
